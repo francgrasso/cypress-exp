c2b759c46dd164cafb7a3149f75ada5b3f9e022d
francgrasso <francgrasso@gmail.com>
2023-06-20 15:24:00 +0200

    first test for gift_cards

---
+++ cypress/e2e/gift-cards/open-sandbox.cy.js
@@ -0,0 +1,18 @@
+describe('Create sandbox', () => {
+    it('Opens a new sandbox successfully', () => {
+        // Visit the WordPress site
+        cy.visit('https://plugins.yithemes.com/yith-woocommerce-gift-cards/');
+        
+        // Accept the cookie banner
+        cy.get('.iubenda-cs-accept-btn').click();
+
+        // Click the open sandbox banner
+        cy.get('a[data-action="ywtenv_create_sandbox"]').click();
+
+        // Set sandbox language to English
+        cy.get('.language-switcher-wrapper div[data-lang="en"]').click();
+        
+        // Check if sandbox is opened
+        cy.contains('Your demo ends in:').should('exist');
+    });
+});
